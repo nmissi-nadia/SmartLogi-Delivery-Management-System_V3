0d01494cadbc73af5bf4794240b94aaa67e2c601
nmissi-nadia <nmissinadia@gmail.com>
2026-01-26 16:07:16 +0100

    configuration cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'cypress';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
 
 async function setupNodeEvents(
     on: Cypress.PluginEvents,
@@ -7,6 +9,14 @@ async function setupNodeEvents(
 ): Promise<Cypress.PluginConfigOptions> {
     // This is required for the preprocessor to be able to generate JSON reports after each run
     await addCucumberPreprocessorPlugin(on, config);
+
+    // Configure esbuild to handle .feature files and step definitions
+    on(
+        'file:preprocessor',
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        })
+    );
 
     // Make sure to return the config object as it might have been modified by the plugin.
     return config;
